79f5e0afa1b9881db2b4e056b09ee1058a3eae92
Bianca <124027117+byanka8@users.noreply.github.com>
2024-12-02 22:35:07 +0800

    Updated c

--- Project/cDistance.c
+++ Project/cDistance.c
@@ -9,44 +9,41 @@ extern double testing();
 int main() {
 	
 	int size, i, j;
-	double x1[MAX][2], y1[MAX][2], x2[MAX][2], y2[MAX][2];
+	// double x1[MAX][2], y1[MAX][2], x2[MAX][2], y2[MAX][2];
 	double z[MAX];
 	double m[MAX];
 	
 	printf("input vector length: ");
 	scanf("%d", &size);
 	
-	// input x1 points values
-	printf("Enter x1 values: ");
-	for (i=0 ; i<size; i++) {
-		scanf("%lf", &x1[i][0]);
-	}
-	
-	printf("Enter x2 values: ");
-	// input x2 points values
-	for (i=0 ; i<size; i++) {
-		scanf("%lf", &x2[i][0]);
-	}
-	
-	// input y1 points values
-	printf("Enter y1 values: ");
-	for (i=0 ; i<size; i++) {
-		scanf("%lf", &y1[i][1]);
-	}
-	
-	// input y2 points values
-	printf("Enter y2 values: ");
-	for (i=0 ; i<size; i++) {
-		scanf("%lf", &y2[i][1]);
-	}
+	size = 4;
+	double x1[] = {1.5, 4.0, 3.5, 2.0};
+    double x2[] = {3.0, 2.5, 2.5, 1.0};
+    double y1[] = {4.0, 3.0, 3.5, 3.0};
+    double y2[] = {2.0, 2.5, 1.0, 1.5};
 	
 	for (i=0 ; i<size; i++) {
-			// compute distance of each point
-			z[i] = compute(x1[i][0], y1[i][1], x2[i][0], y2[i][1]);
-			m[i] = testing(x1[i][0], y1[i][1], x2[i][0], y2[i][1]);
+			// compute distance of each point c
+			z[i] = compute(x1[i], y1[i], x2[i], y2[i]);
 	}
 	
 	// output first 10 elements of z
+	for (i=0 ; i<size && i<10; i++) {
+		// compute distance of each point
+		printf("%.2f", z[i]);
+		if (i<size-1 && i<9) {
+			printf(", ");
+		} 
+	}
+
+	printf("\n");
+
+	for (i=0 ; i<size; i++) {
+			// compute distance of each point asm
+			m[i] = compute(x1[i], y1[i], x2[i], y2[i]);
+	}
+
+	// output first 10 elements of mm
 	for (i=0 ; i<size && i<10; i++) {
 		// compute distance of each point
 		printf("%.2f", m[i]);
